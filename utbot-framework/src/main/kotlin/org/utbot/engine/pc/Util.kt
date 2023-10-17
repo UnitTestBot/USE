eb6ea89da022c0f34c14e81d2a386599a23c99bc
Denis Fokin <Denis.Fokin@gmail.com>
2023-10-17 12:40:48 +0300

    Bring z3 back

--- utbot-framework/src/main/kotlin/org/utbot/engine/pc/Util.kt
+++
@@ -1,7 +0,0 @@
-package org.utbot.engine.pc
-
-import com.microsoft.z3.Expr
-import com.microsoft.z3.Sort
-
-@Suppress("UNCHECKED_CAST")
-fun <T : Sort, R : Sort> Expr<T>.cast(): Expr<R> = this as Expr<R>
